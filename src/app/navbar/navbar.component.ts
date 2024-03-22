a3137447be630e64463249d5545098d006386861
sergiolopezzl <slopez1660@gmail.com>
2024-03-21 23:23:04 -0500

    addContainers

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import $ from 'jquery';
 declare var bootstrap: any;
@@ -10,21 +10,21 @@ declare var bootstrap: any;
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private renderer: Renderer2, private router: Router) { }
 
   ngOnInit(): void {
     $(document).ready(() => {
       // Highlight the active link
       $('.navbar-nav .nav-link').on('click', (e: Event) => {
         $('.navbar-nav .nav-link').removeClass('active');
-        $(e.target as HTMLElement).addClass('active');
+        this.renderer.addClass(e.target as HTMLElement, 'active');
       });
 
       // Close the dropdown menu when a link is clicked
       $('.navbar-nav>li>a').on('click', (e: Event) => {
         const collapse = $(e.target as HTMLElement).closest('.collapse');
         if (collapse.hasClass('show')) {
-          collapse.removeClass('show');
+          this.renderer.removeClass(collapse.get(0), 'show');
         }
       });
 
@@ -32,7 +32,7 @@ export class NavbarComponent implements OnInit {
       $('.navbar-toggler').on('click', () => {
         const collapse = $('#navbarNav');
         if (collapse.hasClass('show')) {
-          collapse.removeClass('show');
+          this.renderer.removeClass(collapse.get(0), 'show');
         }
       });
     });
